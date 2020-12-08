e9aa939a4edb4dc83acfa7ca7132089d770b68df
Maria Mokbel <mmokbel@octo.com>
2020-12-08 09:49:23 +0100

    Solution for day 8

--- day_08/solution.py
+++ day_08/solution.py
@@ -1,48 +1,80 @@
 from functools import reduce
-from typing import List
+from typing import List, Tuple
 
 
-def parse_input(filename: str) -> List[str]:
+def parse_input(filename: str) -> List[Tuple]:
     file_content = []
     with open(filename) as f:
         for line in f:
-            file_content.append(line.replace('\n', ''))
+            cleaned_line = line.replace('\n', '')
+            file_content.append((cleaned_line.split()[0], int(cleaned_line.split()[1])))
     return file_content
 
 
-def get_groups(file_content: List[str]) -> List[str]:
-    groups = []
-    concat = ''
-    for el in file_content:
-        if el == '':
-            groups.append(concat)
-            concat = ''
-            continue
-        concat = concat + el + ' '
-    groups.append(concat)
-    return groups
+def find_accumulator_before_loop(instructions: List[Tuple]) -> int:
+    acc = 0
+    rerun = False
+    index = 0
+    visited = []
+    while not rerun:
+        if index in visited:
+            return acc
+        else:
+            visited.append(index)
+        if instructions[index][0] == 'acc':
+            acc += instructions[index][1]
+            index += 1
+        elif instructions[index][0] == 'nop':
+            index += 1
+        else:
+            index += instructions[index][1]
 
 
-def count_yes_questions(groups: List[str]) -> int:
-    return reduce(lambda a, b: a + b, [len(set(questions.replace(' ', ''))) for questions in groups])
+def find_acc_with_changed_instruction(instructions: List[Tuple]) -> int or None:
+    acc = 0
+    rerun = False
+    index = 0
+    visited = []
+    while not rerun:
+        if index == len(instructions):
+            return acc
+        if index in visited:
+            return None
+        else:
+            visited.append(index)
+
+        if instructions[index][0] == 'acc':
+            acc += instructions[index][1]
+            index += 1
+        elif instructions[index][0] == 'nop':
+            index += 1
+        else:
+            index += instructions[index][1]
 
 
-def count_all_yes_questions(groups: List[str]) -> int:
-    all_yes_questions = 0
-    for group in groups:
-        questions = []
-        people = group.count(' ')
-        for q in group:
-            if group.replace(' ','').count(q) == people and q not in questions:
-                questions.append(q)
-        all_yes_questions += len(questions)
-    return all_yes_questions
+def change_jmp_instruction(instructions, current_index, index_of_changed_instruction, changed_instruction):
+    if index_of_changed_instruction:
+        instructions[index_of_changed_instruction] = changed_instruction
+    if instructions[current_index][0] == 'jmp':
+        changed_instruction = instructions[current_index]
+        instructions[current_index] = ('nop', 0)
+        index_of_changed_instruction = current_index
+    return index_of_changed_instruction, changed_instruction
+
+
+def fix_loop(instructions: List[Tuple]) -> int:
+    index_of_changed_instruction = None
+    changed_instruction = None
+    for i in range(len(instructions)):
+        index_of_changed_instruction, changed_instruction = change_jmp_instruction(instructions, i,
+                                                                                   index_of_changed_instruction,
+                                                                                   changed_instruction)
+        acc = find_acc_with_changed_instruction(instructions)
+        if acc:
+            return acc
 
 
 if __name__ == "__main__":
     content = parse_input('input.txt')
-    groups = get_groups(content)
-    yes_count = count_yes_questions(groups)
-    print("Solution 1:", yes_count)
-    all_yes_count = count_all_yes_questions(groups)
-    print("Solution 2:", all_yes_count)
+    print("Solution 1:", find_accumulator_before_loop(content))
+    print("Solution 2:", fix_loop(content))
